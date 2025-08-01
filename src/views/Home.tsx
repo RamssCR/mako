51e3737568e81854f279924eb32948ba36d0b158
RamssCR <ramsesgpm@gmail.com>
2025-07-31 22:26:01 -0500

    feat: created main view

---
+++ src/views/Home.tsx
@@ -0,0 +1,14 @@
+/**
+ * Home component for the application.
+ * This component serves as the main landing page of the application.
+ * @returns JSX Element representing the Home component.
+ */
+const Home = () => {
+  return (
+    <section className="w-full flex flex-col items-start gap-4">
+      <h1>Mako</h1>
+    </section>
+  )
+}
+
+export default Home
