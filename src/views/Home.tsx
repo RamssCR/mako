dff4ebe57cfa6d6df71a5ce071f37ad7ddb7e08c
RamssCR <ramsesgpm@gmail.com>
2025-08-02 16:55:51 -0500

    feat: added the Dialogue Box to thee Home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,5 @@
+import { DialogueBox } from "@components/dialogue-box/DialogueBox"
+
 /**
  * Home component for the application.
  * This component serves as the main landing page of the application.
@@ -7,6 +9,9 @@ const Home = () => {
   return (
     <section className="w-full flex flex-col items-start gap-4">
       <h1>Mako</h1>
+      <article className="w-full flex items-center justify-center">
+        <DialogueBox />
+      </article>
     </section>
   )
 }
